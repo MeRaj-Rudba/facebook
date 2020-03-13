f53b7a9bbe7fecf3f095845bcd9ff33c332d40a9
MeRaj Rudba <mrudba@gmail.com>
2020-03-13 14:06:01 +0600

    Starting the project

---
+++ src/profiles/info.js
@@ -0,0 +1,134 @@
+
+var info =
+[
+    {
+      "id": 1,
+      "name": "Akib Rahman",
+      "income":6500,
+      "work": "Student",
+      "img":"https://drive.google.com/uc?id=1DaA3fc6oyv8KrOKnyFjteqHPNVmE2zH0"
+        
+    },
+    {
+        "id": 2,
+        "name": "Asha Aman",
+        "income":40000,
+        "work": "Radio Person",
+        "img":"https://drive.google.com/uc?id=1NQsuLxrdY-ZtBxwvernD48anVBS6jatL"
+          
+    },
+    {
+        "id": 3,
+        "name": "Bill Gates",
+        "income":104800000000,
+        "work": "Owner of Microsoft",
+        "img":"https://drive.google.com/uc?id=1KhV_o5s55pIDUC_hpM7H9s7FLqPjF_zL"
+          
+    },
+    {
+      "id": 4,
+      "name": "Brad Pitt",
+      "income":300000000,
+      "work": "Actor",
+      "img":"https://drive.google.com/uc?id=1kKKWIuFyt6WsWAPYyIINj4FNzuUhCGlZ"
+        
+    },
+    {
+        "id": 5,
+        "name": "Foysol Ahmed",
+        "income":55000,
+        "work": "Vagabond",
+        "img":"https://drive.google.com/uc?id=1I5f6y6AWPIQNXqdqM8UQfcVy7QDv-ZMS"
+          
+      },
+    {
+        "id": 6,
+        "name": "Joaquin Phoenix",
+        "income":35000000,
+        "work": "Actor",
+        "img":"https://drive.google.com/uc?id=1fJjqvNjVX4hKRxkGwbwhMrHsLqHdxy3h"
+          
+    },
+    {
+        "id": 7,
+        "name": "Leonardo DeCaprio",
+        "income":260000000,
+        "work": "Actor",
+        "img":"https://drive.google.com/uc?id=1qQllyCtCBNTXUnACZzjJ6WQ7O_kA5S3C"
+          
+    },
+    {
+        "id": 8,
+        "name": "Mahmud Akik",
+        "income":9000,
+        "work": "Student",
+        "img":"https://drive.google.com/uc?id=1MPJJeFxmTyv0g2KgXIq35IrwHa5NiJWV"
+          
+    },
+    {
+        "id": 9,
+        "name": "Mark Zuckerberg",
+        "income":66000000000,
+        "work": "Owner of Facebook",
+        "img":"https://drive.google.com/uc?id=1FI6OygtM5ciDy-NDLWYZshMn4niTrq0X"
+          
+    },
+    {
+        "id": 10,
+        "name": "Mayhadee Hasan Khan",
+        "income":9000,
+        "work": "Creative Architect",
+        "img":"https://drive.google.com/uc?id=1dWd4x198L5PQR8jZFncvm2n8awFOabFU"
+          
+    },
+    {
+        "id": 11,
+        "name": "Mehedi Hasan Rony",
+        "income":7500,
+        "work": "Nobody Knows",
+        "img":"https://drive.google.com/uc?id=1a2iok-rqAZlVm4ZMcbEB8oEkg38U6ikk"
+          
+    },
+    {
+        "id": 12,
+        "name": "Nowshin Sabrin",
+        "income":15000,
+        "work": "Model",
+        "img":"https://drive.google.com/uc?id=1mPZ_OxmKWTmn_qceneMWwT_LiJ6u9e16"
+          
+    },
+    {
+        "id": 13,
+        "name": "Sahrirar Sijan",
+        "income":50000,
+        "work": "Chef",
+        "img":"https://drive.google.com/uc?id=1VEsrhhGiQTZckOsOnpmHsqLeFOoYe7uZ"
+          
+    },
+    {
+        "id": 14,
+        "name": "Salwa Siraj Khan",
+        "income":6000,
+        "work": "Lyricist",
+        "img":"https://drive.google.com/uc?id=1Dh5_ZO4NVOP8esGvkV_9nwlNFyecpBYx"
+          
+    },
+    {
+        "id": 15,
+        "name": "Waly Khan",
+        "income":15000,
+        "work": "Still Confused",
+        "img":"https://drive.google.com/uc?id=18jQU2ahvRozXjoDzLYvAiGduUXZ8drjn"
+          
+    }];
+
+export default info;
+
+
+
+
+
+
+
+
+
